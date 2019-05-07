7e8a491f97ca337833d69bb7cecd218b98ed297e
huangzp <1782336129@qq.com>
2019-05-07 11:18:23 +0800

    first commit

---
+++ src/main/java/com/example/testgit/Hello.java
@@ -0,0 +1,11 @@
+package com.example.testgit;
+
+/**
+ * @author huangzp
+ * @date 2019/5/7 11:11
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
